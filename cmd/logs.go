6f5cc3fdfbf0597f7864f2121a378d535a8191e5
Darren Shepherd <darren@rancher.com>
2016-09-13 01:37:14 -0700

    Add --service to logs command

--- cmd/logs.go
+++ cmd/logs.go
@@ -5,7 +5,9 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"strings"
 	"sync"
+	"time"
 
 	"golang.org/x/net/context"
 
@@ -14,7 +16,9 @@ import (
 	dclient "github.com/docker/engine-api/client"
 	"github.com/docker/engine-api/types"
 	"github.com/docker/libcompose/cli/logger"
-	"github.com/rancher/go-rancher/client"
+	"github.com/mitchellh/mapstructure"
+	"github.com/rancher/cli/monitor"
+	"github.com/rancher/go-rancher/v2"
 	"github.com/rancher/rancher-docker-api-proxy"
 	"github.com/urfave/cli"
 )
@@ -26,9 +30,17 @@ func LogsCommand() cli.Command {
 		Name:        "logs",
 		Usage:       "Fetch the logs of a container",
 		Description: "\nExample:\n\t$ rancher logs web\n",
-		ArgsUsage:   "[CONTAINERNAME CONTAINERID...]",
+		ArgsUsage:   "[CONTAINERNAME CONTAINERID...] or [SERVICENAME SERVICEID...]",
 		Action:      logsCommand,
 		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:  "service,s",
+				Usage: "Show service logs",
+			},
+			cli.BoolTFlag{
+				Name:  "sub-log",
+				Usage: "Show service sub logs",
+			},
 			cli.BoolFlag{
 				Name:  "follow,f",
 				Usage: "Follow log output",
@@ -42,10 +54,6 @@ func LogsCommand() cli.Command {
 				Name:  "since",
 				Usage: "Show logs since timestamp",
 			},
-			//cli.BoolFlag{
-			//	Name:  "details",
-			//	Usage: "Show extra details provided to logs",
-			//},
 			cli.BoolFlag{
 				Name:  "timestamps,t",
 				Usage: "Show timestamps",
@@ -54,12 +62,129 @@ func LogsCommand() cli.Command {
 	}
 }
 
+func printPastLogs(c *client.RancherClient, nameCache map[string]string, services map[string]bool, ctx *cli.Context) (map[string]bool, error) {
+	printed := map[string]bool{}
+
+	listOpts := defaultListOpts(nil)
+	listOpts.Filters["sort"] = "id"
+	listOpts.Filters["order"] = "desc"
+	if !ctx.Bool("sub-log") {
+		listOpts.Filters["subLog"] = "0"
+	}
+
+	limit := ctx.Int("tail")
+	if limit == 0 {
+		return printed, nil
+	}
+
+	if limit > 0 {
+		listOpts.Filters["limit"] = limit
+	}
+
+	logs, err := c.ServiceLog.List(listOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := len(logs.Data); i > 0; i-- {
+		l := logs.Data[i-1]
+		printed[l.Id] = true
+		printServiceLog(c, nameCache, services, l)
+	}
+
+	return printed, nil
+}
+
+func printServiceLog(c *client.RancherClient, nameCache map[string]string, services map[string]bool, log client.ServiceLog) {
+	if len(services) > 0 && !services[log.ServiceId] {
+		return
+	}
+
+	created, _ := time.Parse(time.RFC3339, log.Created)
+	endTime, _ := time.Parse(time.RFC3339, log.EndTime)
+	duration := endTime.Sub(created)
+	durationStr := duration.String()
+	if durationStr == "0" || strings.HasPrefix(durationStr, "-") {
+		durationStr = "-"
+	}
+	if log.EndTime == "" {
+		durationStr = "?"
+	}
+	if log.InstanceId == "" {
+		log.InstanceId = "-"
+	}
+
+	if nameCache[log.ServiceId] == "" {
+		service, err := c.Service.ById(log.ServiceId)
+		if nameCache[service.StackId] == "" {
+			stack, err := c.Stack.ById(service.StackId)
+			if err == nil {
+				nameCache[service.StackId] = stack.Name
+			}
+		}
+		if err == nil {
+			nameCache[log.ServiceId] = service.Name
+		}
+		nameCache[log.ServiceId] = fmt.Sprintf("%s/%s(%s)", nameCache[service.StackId], nameCache[log.ServiceId], log.ServiceId)
+	}
+
+	fmt.Printf("%s %4s %s %s %s %6s %s: %s\n", log.Created, durationStr, strings.SplitN(log.TransactionId, "-", 2)[0],
+		strings.ToUpper(log.Level), nameCache[log.ServiceId], log.InstanceId, log.EventType, log.Description)
+}
+
+func serviceLogs(c *client.RancherClient, ctx *cli.Context) error {
+	nameCache := map[string]string{}
+	var sub *monitor.Subscription
+	follow := ctx.Bool("follow")
+
+	if follow {
+		m := monitor.New(c)
+		sub = m.Subscribe()
+		go func() {
+			logrus.Fatal(m.Start())
+		}()
+	}
+
+	services, err := resolveServices(c, ctx.Args())
+	if err != nil {
+		return err
+	}
+
+	printed, err := printPastLogs(c, nameCache, services, ctx)
+	if err != nil {
+		return err
+	}
+
+	if follow {
+		for event := range sub.C {
+			if event.ResourceType != "serviceLog" {
+				continue
+			}
+			if printed[event.ResourceID] {
+				continue
+			}
+			var log client.ServiceLog
+			err := mapstructure.Decode(event.Data["resource"], &log)
+			if err != nil {
+				logrus.Errorf("Failed to convert %#v: %v", event.Data["resource"], err)
+			}
+			printServiceLog(c, nameCache, services, log)
+		}
+	}
+
+	return nil
+}
+
 func logsCommand(ctx *cli.Context) error {
 	wg := sync.WaitGroup{}
 
 	c, err := GetClient(ctx)
 	if err != nil {
 		return err
+	}
+
+	if ctx.Bool("service") {
+		return serviceLogs(c, ctx)
 	}
 
 	if len(ctx.Args()) == 0 {
@@ -180,11 +305,23 @@ func (f writerFunc) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+func resolveServices(c *client.RancherClient, names []string) (map[string]bool, error) {
+	services := map[string]bool{}
+	for _, name := range names {
+		resource, err := Lookup(c, name, "service")
+		if err != nil {
+			return nil, err
+		}
+		services[resource.Id] = true
+	}
+	return services, nil
+}
+
 func resolveContainers(c *client.RancherClient, names []string) ([]client.Instance, error) {
 	result := []client.Instance{}
 
 	for _, name := range names {
-		resource, err := Lookup(c, name, "container", "service", "environment")
+		resource, err := Lookup(c, name, "container", "service", "stack")
 		if err != nil {
 			return nil, err
 		}
