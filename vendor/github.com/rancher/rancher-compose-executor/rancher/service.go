5140ad2c34c9100be53f1da61016a048a5f8a273
Denise Schannon <denise@rancher.com>
2017-05-29 20:50:58 -0700

    vendor updates

--- vendor/github.com/rancher/rancher-compose-executor/rancher/service.go
+++ vendor/github.com/rancher/rancher-compose-executor/rancher/service.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"strings"
 	"sync"
+	"time"
 
 	"golang.org/x/net/context"
 
@@ -157,6 +158,21 @@ func (r *RancherService) up(create bool) error {
 		err = r.Wait(service)
 	}
 
+	// TODO: revisit whether this is the best place to perform this check
+	if _, ok := r.serviceConfig.Labels["io.rancher.service.wait_for_healthcheck"]; ok {
+		logrus.Debugf("Detected label io.rancher.service.wait_for_healthcheck. Polling for health")
+		for {
+			logrus.Debugf("Service %s has health state %s", service.Name, service.HealthState)
+			if service.HealthState == "healthy" {
+				break
+			}
+			time.Sleep(150 * time.Millisecond)
+			err := r.context.Client.Reload(&service.Resource, service)
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return err
 }
 
