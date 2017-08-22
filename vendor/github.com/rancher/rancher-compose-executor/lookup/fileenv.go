07d2cd8a6498d9117d4a0c0cd4c2cfa13693e11e
Amy Chen <amy@rancher.com>
2017-08-22 16:31:05 -0700

    Update vendor: rancher-compose-executor v0.14.16

--- vendor/github.com/rancher/rancher-compose-executor/lookup/fileenv.go
+++ vendor/github.com/rancher/rancher-compose-executor/lookup/fileenv.go
@@ -2,11 +2,15 @@ package lookup
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
+	yaml "gopkg.in/yaml.v2"
+
 	"github.com/rancher/rancher-compose-executor/config"
 	"github.com/rancher/rancher-compose-executor/utils"
 )
@@ -16,34 +20,85 @@ type FileEnvLookup struct {
 	variables map[string]string
 }
 
-func NewFileEnvLookup(file string, parent config.EnvironmentLookup) (*FileEnvLookup, error) {
+func parseMultiLineEnv(file string, parent config.EnvironmentLookup) (*FileEnvLookup, error) {
 	variables := map[string]string{}
 
-	if file != "" {
-		f, err := os.Open(file)
-		if err != nil {
-			return nil, err
-		}
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			t := scanner.Text()
-			parts := strings.SplitN(t, "=", 2)
-			if len(parts) == 1 {
-				variables[parts[0]] = ""
-			} else {
-				variables[parts[0]] = parts[1]
-			}
-		}
+	contents, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
 
-		if scanner.Err() != nil {
-			return nil, scanner.Err()
+	var data map[string]interface{}
+
+	if strings.HasSuffix(file, ".yml") || strings.HasSuffix(file, ".yaml") {
+		yaml.Unmarshal(contents, &data)
+	} else if strings.HasSuffix(file, ".json") {
+		json.Unmarshal(contents, &data)
+	}
+
+	for k, v := range data {
+		if stringValue, ok := v.(string); ok {
+			variables[k] = stringValue
+		} else if intValue, ok := v.(int); ok {
+			variables[k] = fmt.Sprintf("%v", intValue)
+		} else if int64Value, ok := v.(int64); ok {
+			variables[k] = fmt.Sprintf("%v", int64Value)
+		} else if float32Value, ok := v.(float32); ok {
+			variables[k] = fmt.Sprintf("%v", float32Value)
+		} else if float64Value, ok := v.(float64); ok {
+			variables[k] = fmt.Sprintf("%v", float64Value)
+		} else if boolValue, ok := v.(bool); ok {
+			variables[k] = strconv.FormatBool(boolValue)
+		} else {
+			return nil, fmt.Errorf("Environment variables must be of type string, bool, or int. Key %s is of type %T", k, v)
 		}
 	}
 
-	logrus.Debugf("Environment Context from file %s: %v", file, variables)
 	return &FileEnvLookup{
 		parent:    parent,
 		variables: variables,
+	}, nil
+}
+
+func parseCustomEnvFile(file string, parent config.EnvironmentLookup) (*FileEnvLookup, error) {
+	variables := map[string]string{}
+
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		t := scanner.Text()
+		parts := strings.SplitN(t, "=", 2)
+		if len(parts) == 1 {
+			variables[parts[0]] = ""
+		} else {
+			variables[parts[0]] = parts[1]
+		}
+	}
+
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	return &FileEnvLookup{
+		parent:    parent,
+		variables: variables,
+	}, nil
+}
+
+func NewFileEnvLookup(file string, parent config.EnvironmentLookup) (*FileEnvLookup, error) {
+	if file != "" {
+		if strings.HasSuffix(file, ".yml") || strings.HasSuffix(file, ".yaml") || strings.HasSuffix(file, ".json") {
+			return parseMultiLineEnv(file, parent)
+		}
+		return parseCustomEnvFile(file, parent)
+	}
+
+	return &FileEnvLookup{
+		parent:    parent,
+		variables: map[string]string{},
 	}, nil
 }
 
