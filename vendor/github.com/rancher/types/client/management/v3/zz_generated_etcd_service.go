fb34a1c74f4d14209c2a29d50468f2965a9c593a
Dan Ramich <daniel@rancher.com>
2018-02-20 15:07:48 -0700

    Vendor update

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_etcd_service.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_etcd_service.go
@@ -1,12 +1,22 @@
 package client
 
 const (
-	ETCDServiceType           = "etcdService"
-	ETCDServiceFieldExtraArgs = "extraArgs"
-	ETCDServiceFieldImage     = "image"
+	ETCDServiceType              = "etcdService"
+	ETCDServiceFieldCACert       = "caCert"
+	ETCDServiceFieldCert         = "cert"
+	ETCDServiceFieldExternalURLs = "externalUrls"
+	ETCDServiceFieldExtraArgs    = "extraArgs"
+	ETCDServiceFieldImage        = "image"
+	ETCDServiceFieldKey          = "key"
+	ETCDServiceFieldPath         = "path"
 )
 
 type ETCDService struct {
-	ExtraArgs map[string]string `json:"extraArgs,omitempty"`
-	Image     string            `json:"image,omitempty"`
+	CACert       string            `json:"caCert,omitempty"`
+	Cert         string            `json:"cert,omitempty"`
+	ExternalURLs []string          `json:"externalUrls,omitempty"`
+	ExtraArgs    map[string]string `json:"extraArgs,omitempty"`
+	Image        string            `json:"image,omitempty"`
+	Key          string            `json:"key,omitempty"`
+	Path         string            `json:"path,omitempty"`
 }
