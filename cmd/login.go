306a81a5463f3cd09126721d78ef61b2bfdc4cfc
Dan Ramich <daniel@rancher.com>
2018-03-22 11:43:17 -0700

    Change to login

--- cmd/login.go
+++ cmd/login.go
@@ -2,12 +2,18 @@ package cmd
 
 import (
 	"bufio"
+	"crypto/tls"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 	"strings"
 
+	"github.com/grantae/certinfo"
 	"github.com/rancher/cli/cliclient"
 	"github.com/rancher/cli/config"
 	managementClient "github.com/rancher/types/client/management/v3"
@@ -18,6 +24,11 @@ type LoginData struct {
 	Project     managementClient.Project
 	Index       int
 	ClusterName string
+}
+
+type CACertResponse struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
 }
 
 func LoginCommand() cli.Command {
@@ -59,7 +70,7 @@ func loginSetup(ctx *cli.Context) error {
 
 	path := ctx.GlobalString("cf")
 	if path == "" {
-		path = os.ExpandEnv("${HOME}/.rancher/cli.json")
+		path = os.ExpandEnv("${HOME}/.rancher/cli2.json")
 	}
 
 	cf, err := loadConfig(path)
@@ -75,7 +86,7 @@ func loginSetup(ctx *cli.Context) error {
 	serverConfig := &config.ServerConfig{}
 
 	if ctx.NArg() == 0 {
-		return errors.New("one server is required")
+		return cli.ShowCommandHelp(ctx, "login")
 	}
 	serverConfig.URL = ctx.Args().First()
 
@@ -119,7 +130,16 @@ func loginSetup(ctx *cli.Context) error {
 func getDefaultProject(ctx *cli.Context, cf *config.ServerConfig) (string, error) {
 	mc, err := cliclient.NewManagementClient(cf)
 	if nil != err {
-		return "", err
+		if _, ok := err.(*url.Error); ok && strings.Contains(err.Error(), "certificate signed by unknown authority") {
+			// no cert was provided and it's most likely a self signed cert if
+			// we get here so grab the cacert and see if the user accepts the server
+			mc, err = getCertFromServer(cf)
+			if nil != err {
+				return "", err
+			}
+		} else {
+			return "", err
+		}
 	}
 
 	projectCollection, err := mc.ManagementClient.Project.List(defaultListOpts(ctx))
@@ -130,6 +150,10 @@ func getDefaultProject(ctx *cli.Context, cf *config.ServerConfig) (string, error
 	if len(projectCollection.Data) == 0 {
 		fmt.Println("There are no projects in the cluster, please create one and login again")
 		return "", nil
+	}
+
+	if len(projectCollection.Data) == 1 {
+		return projectCollection.Data[0].ID, nil
 	}
 
 	clusterNames, err := getClusterNames(ctx, mc)
@@ -145,24 +169,24 @@ func getDefaultProject(ctx *cli.Context, cf *config.ServerConfig) (string, error
 		{"PROJECT DESCRIPTION", "Project.Description"},
 	}, ctx)
 
-	fmt.Println("Select your default Project:")
 	for i, item := range projectCollection.Data {
 		writer.Write(&LoginData{
 			Project:     item,
-			Index:       i,
+			Index:       i + 1,
 			ClusterName: clusterNames[item.ClusterId],
 		})
 	}
 
 	writer.Close()
-
 	if nil != writer.Err() {
 		return "", writer.Err()
 	}
 
+	fmt.Print("Select your default Project:")
+
 	reader := bufio.NewReader(os.Stdin)
 
-	errMessage := fmt.Sprintf("invalid input, enter a number between 0 and %v", len(projectCollection.Data)-1)
+	errMessage := fmt.Sprintf("Invalid input, enter a number between 1 and %v: ", len(projectCollection.Data))
 	var selection int
 
 	for {
@@ -175,18 +199,100 @@ func getDefaultProject(ctx *cli.Context, cf *config.ServerConfig) (string, error
 		if input != "" {
 			i, err := strconv.Atoi(input)
 			if nil != err {
-				fmt.Println(errMessage)
+				fmt.Print(errMessage)
 				continue
 			}
-			if i <= len(projectCollection.Data)-1 {
-				selection = i
+			if i <= len(projectCollection.Data) && i != 0 {
+				selection = i - 1
 				break
 			}
-			fmt.Println(errMessage)
+			fmt.Print(errMessage)
 			continue
 		}
 	}
 	return projectCollection.Data[selection].ID, nil
+}
+
+func getCertFromServer(cf *config.ServerConfig) (*cliclient.MasterClient, error) {
+	req, err := http.NewRequest("GET", cf.URL+"/v3/settings/cacerts", nil)
+	if nil != err {
+		return nil, err
+	}
+
+	req.SetBasicAuth(cf.AccessKey, cf.SecretKey)
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	res, err := client.Do(req)
+	if nil != err {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	content, err := ioutil.ReadAll(res.Body)
+	if nil != err {
+		return nil, err
+	}
+
+	var certReponse *CACertResponse
+	err = json.Unmarshal(content, &certReponse)
+
+	cert, err := verifyCert([]byte(certReponse.Value))
+	if nil != err {
+		return nil, err
+	}
+
+	// Get the server cert chain in a printable form
+	serverCerts, err := processServerChain(res)
+	if nil != err {
+		return nil, err
+	}
+
+	if ok := verifyUserAcceptsCert(serverCerts, cf.URL); ok {
+		cf.CACerts = cert
+	} else {
+		return nil, errors.New("CACert of server was not accepted, unable to login")
+	}
+
+	return cliclient.NewManagementClient(cf)
+}
+
+func verifyUserAcceptsCert(certs []string, url string) bool {
+	fmt.Printf("The authenticity of server '%s' can't be established.\n", url)
+	fmt.Printf("Cert chain is : %v \n", certs)
+	fmt.Print("Do you want to continue connecting (yes/no)? ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		input = strings.ToLower(strings.TrimSpace(input))
+
+		if input == "yes" || input == "y" {
+			return true
+		} else if input == "no" || input == "n" {
+			return false
+		}
+		fmt.Printf("Please type 'yes' or 'no': ")
+	}
+	return false
+}
+
+func processServerChain(res *http.Response) ([]string, error) {
+	var allCerts []string
+
+	for _, cert := range res.TLS.PeerCertificates {
+		result, err := certinfo.CertificateText(cert)
+		if err != nil {
+			return allCerts, err
+		}
+		allCerts = append(allCerts, result)
+	}
+	return allCerts, nil
 }
 
 func loginContext(ctx *cli.Context) error {
