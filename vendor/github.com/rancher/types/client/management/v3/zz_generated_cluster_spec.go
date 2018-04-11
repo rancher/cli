e8c60d5d95ff57b0500ad045888ab2882c2a1b38
Dan Ramich <daniel@rancher.com>
2018-04-11 10:03:33 -0700

    Vendor update

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_cluster_spec.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_cluster_spec.go
@@ -2,11 +2,13 @@ package client
 
 const (
 	ClusterSpecType                                     = "clusterSpec"
+	ClusterSpecFieldAmazonElasticContainerServiceConfig = "amazonElasticContainerServiceConfig"
 	ClusterSpecFieldAzureKubernetesServiceConfig        = "azureKubernetesServiceConfig"
 	ClusterSpecFieldDefaultClusterRoleForProjectMembers = "defaultClusterRoleForProjectMembers"
 	ClusterSpecFieldDefaultPodSecurityPolicyTemplateId  = "defaultPodSecurityPolicyTemplateId"
 	ClusterSpecFieldDescription                         = "description"
 	ClusterSpecFieldDesiredAgentImage                   = "desiredAgentImage"
+	ClusterSpecFieldDisplayName                         = "displayName"
 	ClusterSpecFieldGoogleKubernetesEngineConfig        = "googleKubernetesEngineConfig"
 	ClusterSpecFieldImportedConfig                      = "importedConfig"
 	ClusterSpecFieldInternal                            = "internal"
@@ -14,13 +16,15 @@ const (
 )
 
 type ClusterSpec struct {
-	AzureKubernetesServiceConfig        *AzureKubernetesServiceConfig  `json:"azureKubernetesServiceConfig,omitempty" yaml:"azureKubernetesServiceConfig,omitempty"`
-	DefaultClusterRoleForProjectMembers string                         `json:"defaultClusterRoleForProjectMembers,omitempty" yaml:"defaultClusterRoleForProjectMembers,omitempty"`
-	DefaultPodSecurityPolicyTemplateId  string                         `json:"defaultPodSecurityPolicyTemplateId,omitempty" yaml:"defaultPodSecurityPolicyTemplateId,omitempty"`
-	Description                         string                         `json:"description,omitempty" yaml:"description,omitempty"`
-	DesiredAgentImage                   string                         `json:"desiredAgentImage,omitempty" yaml:"desiredAgentImage,omitempty"`
-	GoogleKubernetesEngineConfig        *GoogleKubernetesEngineConfig  `json:"googleKubernetesEngineConfig,omitempty" yaml:"googleKubernetesEngineConfig,omitempty"`
-	ImportedConfig                      *ImportedConfig                `json:"importedConfig,omitempty" yaml:"importedConfig,omitempty"`
-	Internal                            bool                           `json:"internal,omitempty" yaml:"internal,omitempty"`
-	RancherKubernetesEngineConfig       *RancherKubernetesEngineConfig `json:"rancherKubernetesEngineConfig,omitempty" yaml:"rancherKubernetesEngineConfig,omitempty"`
+	AmazonElasticContainerServiceConfig *AmazonElasticContainerServiceConfig `json:"amazonElasticContainerServiceConfig,omitempty" yaml:"amazonElasticContainerServiceConfig,omitempty"`
+	AzureKubernetesServiceConfig        *AzureKubernetesServiceConfig        `json:"azureKubernetesServiceConfig,omitempty" yaml:"azureKubernetesServiceConfig,omitempty"`
+	DefaultClusterRoleForProjectMembers string                               `json:"defaultClusterRoleForProjectMembers,omitempty" yaml:"defaultClusterRoleForProjectMembers,omitempty"`
+	DefaultPodSecurityPolicyTemplateId  string                               `json:"defaultPodSecurityPolicyTemplateId,omitempty" yaml:"defaultPodSecurityPolicyTemplateId,omitempty"`
+	Description                         string                               `json:"description,omitempty" yaml:"description,omitempty"`
+	DesiredAgentImage                   string                               `json:"desiredAgentImage,omitempty" yaml:"desiredAgentImage,omitempty"`
+	DisplayName                         string                               `json:"displayName,omitempty" yaml:"displayName,omitempty"`
+	GoogleKubernetesEngineConfig        *GoogleKubernetesEngineConfig        `json:"googleKubernetesEngineConfig,omitempty" yaml:"googleKubernetesEngineConfig,omitempty"`
+	ImportedConfig                      *ImportedConfig                      `json:"importedConfig,omitempty" yaml:"importedConfig,omitempty"`
+	Internal                            bool                                 `json:"internal,omitempty" yaml:"internal,omitempty"`
+	RancherKubernetesEngineConfig       *RancherKubernetesEngineConfig       `json:"rancherKubernetesEngineConfig,omitempty" yaml:"rancherKubernetesEngineConfig,omitempty"`
 }
