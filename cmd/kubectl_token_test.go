2330c334be9f36b4f6fbd4934ff66a5dfce782dc
Gregor Pirolt <gregor.pirolt@willhaben.at>
2025-05-30 11:26:49 +0200

    ensure tests are populated with new flag

--- cmd/kubectl_token_test.go
+++ cmd/kubectl_token_test.go
@@ -126,6 +126,7 @@ func TestCacheCredential(t *testing.T) {
 	flagSet := flag.NewFlagSet("test", 0)
 	flagSet.String("server", "rancher.example.com", "doc")
 	flagSet.String("config", t.TempDir(), "doc")
+	flagSet.String("config-helper", "built-in", "")
 	cliCtx := cli.NewContext(nil, flagSet, nil)
 
 	serverConfig, err := lookupServerConfig(cliCtx)
